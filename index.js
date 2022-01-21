cd6cc6d7570dddfb4e3cc9e93ae4c66a356b8ba7
Alessio Bonti <alexbonti83@hotmail.com>
2022-01-21 11:57:38 +1100

    fixed master

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 //require the express package
 const express = require('express')
-//const port = 4000
-const ports = [5000,6000]
+const port = 4000
 //create an app using the express
 // package
-const servers=[]
+const app = express()
 
-// we create express instances based on how many ports we have
-ports.forEach(port=>{
-    let app=express()
-    servers.push(app)
+app.use('/', express.static('public'))
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
-servers.forEach(server=>{
-    // we  pop one of the ports , this returns its value and removes it from the
-    // array ports, this way we know is not going to be used again
-    let tempPort=ports.pop()
-
-    //we attach the port to the public folder, you could make it 
-    // more complex and have multiple public folders
-    server.use('/', express.static('public'))
-    server.listen(tempPort, () => {
-        console.log(`Example app listening at http://localhost:${tempPort}`)
-    })
-      
-
-})
-
   
