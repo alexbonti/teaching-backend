35bcc078074e7df5f6ab1ed79a0dc8ac71246efa
Alessio Bonti <alexbonti83@hotmail.com>
2022-01-21 12:48:53 +1100

    completed exercise 3

--- routes/calculatorRoute.js
+++ routes/calculatorRoute.js
@@ -8,6 +8,29 @@ router.get('/add', (req, res) => {
     res.status(200)
     res.json({result}) 
 })
+router.get('/subtract', (req, res) => {
+    let number1=parseInt(req.query.num1);
+    let number2=parseInt(req.query.num2);
+    let result=number1-number2;
+    res.status(200)
+    res.json({result}) 
+})
+
+router.get('/multiply', (req, res) => {
+    let number1=parseInt(req.query.num1);
+    let number2=parseInt(req.query.num2);
+    let result=number1*number2;
+    res.status(200)
+    res.json({result}) 
+})
+
+router.get('/divide', (req, res) => {
+    let number1=parseInt(req.query.num1);
+    let number2=parseInt(req.query.num2);
+    let result=number1/number2;
+    res.status(200)
+    res.json({result}) 
+})
 
 
 
