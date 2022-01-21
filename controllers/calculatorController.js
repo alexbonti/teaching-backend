5cd6f1040745dc2b38f4364a213390a1187d4102
Alessio Bonti <alexbonti83@hotmail.com>
2022-01-21 15:33:06 +1100

    solution ex5a

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,8 +1,10 @@
+const Calculator = require('../libraries/Calculator');
+let myCalc=new Calculator()
 
 const addNumbers = (req, res) => {
     let number1=parseInt(req.query.num1);
     let number2=parseInt(req.query.num2);
-    let result=number1+number2;
+    let result=myCalc.add(number1,number2)
     res.status(200)
     res.json({result}) 
 }
@@ -10,7 +12,7 @@ const addNumbers = (req, res) => {
 const subtractNumbers = (req, res) => {
     let number1=parseInt(req.query.num1);
     let number2=parseInt(req.query.num2);
-    let result=number1-number2;
+    let result=myCalc.subtract(number1,number2)
     res.status(200)
     res.json({result}) 
 }
@@ -18,7 +20,7 @@ const subtractNumbers = (req, res) => {
 const multiplyNumbers = (req, res) => {
     let number1=parseInt(req.query.num1);
     let number2=parseInt(req.query.num2);
-    let result=number1*number2;
+    let result=myCalc.multiply(number1,number2)
     res.status(200)
     res.json({result}) 
 }
@@ -26,7 +28,7 @@ const multiplyNumbers = (req, res) => {
 const divideNumbers = (req, res) => {
     let number1=parseInt(req.query.num1);
     let number2=parseInt(req.query.num2);
-    let result=number1/number2;
+    let result=myCalc.divide(number1,number2)
     res.status(200)
     res.json({result}) 
 }
