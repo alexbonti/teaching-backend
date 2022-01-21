f36e689dfe9e86929333da17ccd6d1af6f45822f
Alessio Bonti <alexbonti83@hotmail.com>
2022-01-21 12:57:24 +1100

    completed exercise 4

--- routes/calculatorRoute.js
+++ routes/calculatorRoute.js
@@ -1,35 +1,24 @@
 var express = require('express');
+var calculatorController = require('../controllers/calculatorController')
 var router = express.Router();
 
 router.get('/add', (req, res) => {
-    let number1=parseInt(req.query.num1);
-    let number2=parseInt(req.query.num2);
-    let result=number1+number2;
-    res.status(200)
-    res.json({result}) 
+    calculatorController.addNumbers(req,res)
+
 })
 router.get('/subtract', (req, res) => {
-    let number1=parseInt(req.query.num1);
-    let number2=parseInt(req.query.num2);
-    let result=number1-number2;
-    res.status(200)
-    res.json({result}) 
+    calculatorController.subtractNumbers(req,res)
+
 })
 
 router.get('/multiply', (req, res) => {
-    let number1=parseInt(req.query.num1);
-    let number2=parseInt(req.query.num2);
-    let result=number1*number2;
-    res.status(200)
-    res.json({result}) 
+    calculatorController.multiplyNumbers(req,res)
+
 })
 
 router.get('/divide', (req, res) => {
-    let number1=parseInt(req.query.num1);
-    let number2=parseInt(req.query.num2);
-    let result=number1/number2;
-    res.status(200)
-    res.json({result}) 
+    calculatorController.divideNumbers(req,res)
+
 })
 
 
